d7d1b72a4ad320424fcba6af015ea81848196929
길근우 <kesksjxhx@gmail.com>
2024-05-09 13:42:08 +0900

    🧩 :: user 생성자 추가

--- src/domain/user/model/user.model.ts
+++ src/domain/user/model/user.model.ts
@@ -1,6 +1,14 @@
 export class User {
-    id: number;
+    id: bigint;
     name: string;
     accountId: string;
     password: string;
+
+
+    constructor(name: string, accountId: string, password: string, id?: bigint) {
+        this.id = id ? id : BigInt(0);
+        this.name = name;
+        this.accountId = accountId;
+        this.password = password;
+    }
 }
