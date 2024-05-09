4d8800538a716f473252c14cd5c1054971a518fe
길근우 <kesksjxhx@gmail.com>
2024-05-09 16:24:24 +0900

    🛠️ :: passwordservice로 변경, transaction 제거

--- src/domain/user/service/create-user.service.ts
+++ src/domain/user/service/create-user.service.ts
@@ -2,31 +2,27 @@ import { ConflictException, Inject, Injectable } from '@nestjs/common';
 import { UserRepository } from '../repository/user.repository';
 import { CreateUserRequest } from '../controller/dto/user.request';
 import { User } from '../model/user.model';
-import * as bcrypt from 'bcrypt';
-import { TransactionService } from '../../../global/prisma/transaction.service';
+import { PasswordService } from '../../../global/auth/password.service';
 
 @Injectable()
 export class CreateUserService {
     constructor(
         @Inject(UserRepository)
         private readonly userRepository: UserRepository,
-        private readonly transactionService: TransactionService
+        private readonly passwordService: PasswordService
     ) {
     }
 
     public async execute(request: CreateUserRequest) {
-        return await this.transactionService.transaction(async () => {
-            const user = await this.userRepository.findByAccountId(request.accountId);
-            if (user) {
-                throw new ConflictException('User Already Exists');
-            }
+        const user = await this.userRepository.findByAccountId(request.accountId);
+        if (user) {
+            throw new ConflictException('User Already Exists');
+        }
 
-            await this.userRepository.saveUser(new User(
-                request.name,
-                request.accountId,
-                await bcrypt.hash(request.password, 10)
-            ));
-        });
-
+        await this.userRepository.saveUser(new User(
+            request.name,
+            request.accountId,
+            await this.passwordService.encode(request.password)
+        ));
     }
 }
