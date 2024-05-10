c8532e0d187c9df522f5c7a8f419bf19e45bd483
길근우 <kesksjxhx@gmail.com>
2024-05-10 16:30:52 +0900

    🧩 :: 문제 조회 api

---
+++ src/domain/quiz/controller/quiz.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, HttpCode, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../../../global/auth/guard/jwt.guard';
+import { SolveQuizRequest } from './dto/quiz.request';
+import { CurrentUser } from '../../../global/decorator/current-user.decorator';
+import { User } from '../../user/model/user.model';
+import { SolveQuizService } from '../service/solve-quiz.service';
+import { QueryQuizService } from '../service/query-quiz.service';
+import { SolveQuizResponse } from './dto/quiz.response';
+
+@Controller('quiz')
+export class QuizController {
+    constructor(
+        private readonly createQuizService: SolveQuizService,
+        private readonly queryQuizService: QueryQuizService
+    ) {
+    }
+
+    @UseGuards(JwtGuard)
+    @HttpCode(201)
+    @Post(':wordId')
+    public async createQuiz(
+        @Param('wordId') wordId: bigint,
+        @Body() request: SolveQuizRequest,
+        @CurrentUser() currentUser: User
+    ): Promise<SolveQuizResponse> {
+        return await this.createQuizService.execute(wordId, request, currentUser);
+    }
+
+    @UseGuards(JwtGuard)
+    @Get()
+    public async queryQuiz(
+        @Query('word_id') wordId: bigint,
+        @CurrentUser() currentUser: User
+    ) {
+        return await this.queryQuizService.execute(wordId, currentUser);
+    }
+}
---
+++ src/domain/quiz/service/query-quiz.service.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { QuizRepository } from '../repository/quiz.repository';
+import { WordRepository } from '../../word/repository/word.repository';
+import { User } from '../../user/model/user.model';
+import { QueryQuizResponse } from '../controller/dto/quiz.response';
+
+@Injectable()
+export class QueryQuizService {
+    constructor(
+        @Inject(QuizRepository)
+        private readonly quizRepository: QuizRepository,
+        @Inject(WordRepository)
+        private readonly wordRepository: WordRepository
+    ) {
+    }
+
+    public async execute(wordId: bigint, currentUser: User): Promise<QueryQuizResponse> {
+        const word = wordId ?
+            await this.wordRepository.findById(wordId) :
+            (await this.wordRepository.findRandomWord(currentUser.id))[0];
+        if (!word) {
+            throw new NotFoundException('Word Not Found');
+        }
+
+        const koreans = (await this.wordRepository.findRandomKoreans(currentUser.id, word.korean))
+            .map((korean) => korean.korean);
+        koreans.push(word.korean);
+        koreans.sort(() => Math.random() - 0.5);
+
+        return new QueryQuizResponse(Number(word.id), word.english, koreans);
+    }
+}
