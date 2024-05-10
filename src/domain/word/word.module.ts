79fc01bdfb381f6c816b238c8f18751a6f16b809
길근우 <kesksjxhx@gmail.com>
2024-05-10 09:04:57 +0900

    🗂️ :: word module

--- src/domain/word/word.module.ts
+++ src/domain/word/word.module.ts
@@ -1,6 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { CreateWordService } from './service/create-word.service';
+import { WordController } from './controller/word.controller';
+import { WordRepository } from './repository/word.repository';
+import { WordRepositoryImpl } from './repository/word.repository.impl';
+import { UpdateWordService } from './service/update-word.service';
+
+const WORD_REPOSITORY = { provide: WordRepository, useClass: WordRepositoryImpl };
 
 @Module({})
+@Module({
+    providers: [WORD_REPOSITORY, CreateWordService, UpdateWordService],
+    exports: [WORD_REPOSITORY],
+    controllers: [WordController]
+})
 export class WordModule {
-
 }
