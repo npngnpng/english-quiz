8efe78b4e9d2de08c403ea221848e26239a4c459
길근우 <kesksjxhx@gmail.com>
2024-05-10 16:31:29 +0900

    🗂️ :: quiz 모듈

--- src/domain/quiz/quiz.module.ts
+++ src/domain/quiz/quiz.module.ts
@@ -1,5 +1,16 @@
 import { Module } from '@nestjs/common';
+import { QuizRepository } from './repository/quiz.repository';
+import { QuizRepositoryImpl } from './repository/quiz.repository.impl';
+import { QuizController } from './controller/quiz.controller';
+import { SolveQuizService } from './service/solve-quiz.service';
+import { QueryQuizService } from './service/query-quiz.service';
 
-@Module({})
+const QUIZ_REPOSITORY = { provide: QuizRepository, useClass: QuizRepositoryImpl };
+
+@Module({
+    providers: [QUIZ_REPOSITORY, SolveQuizService, QueryQuizService],
+    exports: [QUIZ_REPOSITORY],
+    controllers: [QuizController]
+})
 export class QuizModule {
 }
