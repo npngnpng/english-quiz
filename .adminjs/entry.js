7e2596995e8989e492d77df94538843c0a0c0769
길근우 <kesksjxhx@gmail.com>
2024-05-13 15:04:52 +0900

    🧩 :: 어드민 웹 구성

---
+++ .adminjs/entry.js
@@ -0,0 +1,7 @@
+AdminJS.UserComponents = {}
+import RelationsShowPropertyComponent from '../node_modules/@adminjs/relations/lib/components/RelationsShowPropertyComponent'
+AdminJS.UserComponents.RelationsShowPropertyComponent = RelationsShowPropertyComponent
+import RelationsEditPropertyComponent from '../node_modules/@adminjs/relations/lib/components/RelationsEditPropertyComponent'
+AdminJS.UserComponents.RelationsEditPropertyComponent = RelationsEditPropertyComponent
+import RelationsListPropertyComponent from '../node_modules/@adminjs/relations/lib/components/RelationsListPropertyComponent'
+AdminJS.UserComponents.RelationsListPropertyComponent = RelationsListPropertyComponent
