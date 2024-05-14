3303ba4eb996dc1032dc06ef889287dc3c2cf091
gilgeunwoo <kesksjxhx@gmail.com>
2024-05-14 15:53:49 +0900

    🧩 :: 캐시 생성 로직 추가

--- src/domain/user/service/create-user.service.ts
+++ src/domain/user/service/create-user.service.ts
@@ -3,6 +3,8 @@ import { UserRepository } from '../repository/user.repository.js';
 import { CreateUserRequest } from '../controller/dto/user.request.js';
 import { User } from '../model/user.model.js';
 import { PasswordEncoder } from '../../../global/auth/password.encoder.js';
+import { CashRepository } from '../../cash/repository/cash.repository.js';
+import { Cash } from '../../cash/model/cash.model.js';
 
 @Injectable()
 export class CreateUserService {
@@ -10,20 +12,22 @@ export class CreateUserService {
         @Inject(UserRepository)
         private readonly userRepository: UserRepository,
         @Inject(PasswordEncoder)
-        private readonly passwordEncoder: PasswordEncoder
+        private readonly passwordEncoder: PasswordEncoder,
+        @Inject(CashRepository)
+        private readonly cashRepository: CashRepository
     ) {
     }
 
     async execute(request: CreateUserRequest): Promise<void> {
-        const user = await this.userRepository.findByAccountId(request.accountId);
-        if (user) {
+        if (await this.userRepository.findByAccountId(request.accountId)) {
             throw new ConflictException('User Already Exists');
         }
 
-        await this.userRepository.saveUser(new User(
+        const user = await this.userRepository.saveUser(new User(
             request.name,
             request.accountId,
             await this.passwordEncoder.encode(request.password)
         ));
+        await this.cashRepository.saveCash(new Cash(user.id));
     }
 }
